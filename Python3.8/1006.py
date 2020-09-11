58da516f58816d08b1e0b075ef6a68b3641e14f8
Pedro Fortes <55544345+pedrofortes4@users.noreply.github.com>
2020-09-11 04:18:35 -0300

    Update 1006.py
    Acrescentei comentários

--- Python3.8/1006.py
+++ Python3.8/1006.py
@@ -3,3 +3,7 @@ b = float(input())
 c = float(input())
 media = ((a*2) + (b*3) + (c*5))/10.0
 print(f'MEDIA = {media:.1f}')
+
+''' OBSERVAÇÕES
+Este código é aceito se você o submete em Python 3.8, mas não em Python 3 (Runtime Error);
+'''
